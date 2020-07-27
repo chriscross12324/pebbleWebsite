a18d18c6f7c6d4aafbbffc9b8d36dee3f0e03221
Chris <chrismc16@outlook.com>
2020-07-27 12:29:19 -0700

    July 27, 2020 - Gradients now show - Gradients are in a grid (responsive)

--- script.js
+++ script.js
@@ -28,12 +28,6 @@ function updateTime() {
   pebbleText.innerText = "The time is: " + hours + ":" + minutes + ":" + seconds;
 }
 
-let gradientModule =
-  `<div class="gradientHolder">
-    <div id="gradientViewer" class="gradientViewer"></div>
-    <h1 id="gradientName" class="gradientName">Green Lagoon</h1>
-  </div>`;
-
 function getGradients() {
   var SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwFkoSBTbmeB6l9iIiZWGczp9sDEjqX0jiYeglczbLKFAXsmtB1/exec";
   $(document).ready(function() {
@@ -42,31 +36,47 @@ function getGradients() {
                 try {
                   gradientJSON = JSON.parse(JSON.stringify(data));
                   pebbleLogo.src = "assets/images/icon_pebble_sunshine.png"
+
+                  let gradientModule =
+                    `<div class="gradientHolder">
+                      <div id="gradientViewer class="gradientViewer></div>
+                      <h1 id="gradientName" class=gradientName></h1>
+                    </div>`;
                   //console.log(gradientJSON);
                   //alert(Object.keys(gradientJSON.items).length);
                   //alert(gradientJSON.items.length);
+                  //var template = document.getElementsByClassName("gradientHolder")[0];
+                  let toAppend = '';
 
                   for (var i = gradientJSON.items.length - 1; i >= 0; i--) {
                     var itemObject = gradientJSON.items[i];
 
-                    var itemq = {}
-                    item["gradientName"] = itemObject["gradientName"];
-                    item["startColour"] = itemObject["startColour"];
-                    item["endColour"] = itemObject["endColour"];
-                    item["description"] = itemObject["description"];
+                    //var div = document.createElement("div");
+                    //template.appendChild(div);
+                    //var h1 = document.createElement("h1");
+                    //h1.innerHTML = itemObject.gradientName;
+                    //template.appendChild(h1);
+                    toAppend +=
+                      `<div class="gradientHolder"><style>div.gradientHolder {margin: auto; position: relative;}</style>
+                        <div id="marginManager" class="marginManager"><style>div.marginManager {margin-top: 120px;}</style>
+                          <div id="gradientViewer${i}" class="gradientViewer${i}"><style>div.gradientViewer${i} {height: 170px; width: 130px; background-image: linear-gradient(145deg, ${itemObject.startColour}, ${itemObject.endColour}); border: none;
+                          border-radius: 20px;
+                          margin: auto;}</style></div>
+                          <h1 id="gradientName" class="gradientName">${itemObject.gradientName}</h1>
+                        </div>
+                      </div>`;
+
+
+                    //var itemq = {}
+                    //item["gradientName"] = itemObject["gradientName"];
+                    //item["startColour"] = itemObject["startColour"];
+                    //item["endColour"] = itemObject["endColour"];
+                    //item["description"] = itemObject["description"];
 
                     //array[i]
                     //console.log(item);
                   }
-                  let item = gradientJSON.reduce((acc, {gradientName}) =>
-                        acc += `
-                        <div class="gradientHolder">
-                          <div id="gradientViewer" class="gradientViewer"></div>
-                          <h1 id="gradientName" class="gradientName">Green Lagoon</h1>
-                        </div>`, ``
-                      );
-
-                $('.content').append(item);
+                  $('.wrapper').empty().append(toAppend);
                 } catch (e) {
                   console.log(e);
                 }
